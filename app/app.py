936d5045347271ba4d4e4ba81170346476eb7774
gespi <gespi@sylvanas-2.local>
2024-08-05 16:09:38 -0400

    some changes

--- app/app.py
+++ app/app.py
@@ -1,25 +1,30 @@
 from flask import Flask, redirect, request, Response
+from flask_cors import CORS
 from lib.routes.route import Route
 import lib.misc.generators as gen
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello():
     return "Hello, World!"
 
+# generate a route to a link
 @app.route("/generate", methods=["POST"])
 def generate():
-    link = request.get_json()["linkTo"]
-    id = gen.generateUniqueID()
-    r = Route(id)
+    link = request.get_json()["linkto"]
+    r = Route()
     r.setLink(link)
     r.createRoute()
+    r.closeDBconn()
     return Response(status="200 OK")
 
+# redirect to route url 
 @app.route("/r/<id>", methods=["GET"])
 def redirect_external(id):
-    r = Route(id)
+    r = Route()
+    r.setId(id)
     link = r.getLink()
     r.closeDBconn()
     return redirect(link, code=302)
--- app/lib/routes/route.py
+++ app/lib/routes/route.py
@@ -1,32 +1,34 @@
 import lib.db.pg_client as pg_client
 
 class Route:
-    def __init__(self, id):
-        self.id = id
+    def __init__(self):
+        self.id = ""
         self.pg_conn = pg_client.connect()
-        self.exist = self.idExist()
         self.link = ""
 
-    def idExist(self):
-        cur = self.pg_conn.cursor()
-        cur.execute(f"SELECT EXISTS(SELECT 1 FROM public.routes WHERE id='{self.id}');")
-        result = cur.fetchone()[0] # returns tuple, extract boolean result
-        cur.close()
-        return bool(result) 
+    # def idExist(self, id):
+    #     cur = self.pg_conn.cursor()
+    #     cur.execute(f"SELECT EXISTS(SELECT 1 FROM public.router WHERE id='{self.id}');")
+    #     result = cur.fetchone()[0] # returns tuple, extract boolean result
+    #     cur.close()
+    #     return bool(result) 
 
     def setLink(self, link):
         self.link = link
 
+    def setId(self, id):
+        self.id = id
+
     def getLink(self):
         cur = self.pg_conn.cursor()
-        cur.execute(f"SELECT linkto FROM public.routes WHERE id='{self.id}';")
+        cur.execute(f"SELECT linkto FROM public.router WHERE id='{self.id}';")
         self.link  = cur.fetchone()[0]
         cur.close()
         return self.link
 
     def createRoute(self):
         cur = self.pg_conn.cursor()
-        cur.execute(f"INSERT INTO public.routes (id, linkto) VALUES ('{self.id}', '{self.link}');")
+        cur.execute(f"INSERT INTO public.router (linkto) VALUES ('{self.link}');")
         cur.close()
         
     def closeDBconn(self):
